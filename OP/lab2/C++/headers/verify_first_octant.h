a3de31675d646274f1ac6d644db582a4441680d9
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-04-16 23:51:01 +0200

    ✔ C++

--- OP/lab2/C++/headers/verify_first_octant.h
+++ OP/lab2/C++/headers/verify_first_octant.h
@@ -3,16 +3,13 @@
 #include "MaterialPoint.h"
 
 /**
- * @brief Calculates in which octant of the 3D space the provided point is.
- * For instance if coordinates are {1, 2, 3}, then the point is in the first octant.
+ * @brief Defines whether the provided point is in the first octant i.e. x, y and z are all positive.
  *
  * @param point material point with defined coordinates
- * @return octant number
+ * @return in the first octant or not
  */
 bool verify_first_octant(MaterialPoint const &point)
 {
     const auto *point_coords = point.get_coords();
-    if (point_coords[0] >= 0 && point_coords[1] >= 0 && point_coords[2] >= 0)
-        return true;
-    return false;
+    return (point_coords[0] >= 0 && point_coords[1] >= 0 && point_coords[2] >= 0) ? true : false;
 }
