a28cf697f1558f64485c48e2c47b7a497a0a9b6c
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-04-16 23:51:48 +0200

    ✔ Python

---
+++ OP/lab2/Python/modules/show_coords_table.py
@@ -0,0 +1,17 @@
+from modules.MaterialPoint import MaterialPoint
+
+
+def show_coords_table(points: list[MaterialPoint], header_title: str):
+    """Displays a table of points' coordinates and vectors.
+
+    Args:
+        points (list[MaterialPoint]): vector of points
+        header_title (str): table title
+    """
+
+    print(f"\n+--+{header_title.center(26, '-')}+")
+
+    for idx, point in enumerate(points):
+        print(f"|{idx+1:>2}| ({point.coords[0]:>6}, {point.coords[1]:>6}, {point.coords[2]:>6}) |")
+
+    print("+--+--------------------------+")
