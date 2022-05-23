922861c5146be3ddfbdfd7e292aaa68dc0210487
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-05-23 21:36:04 +0200

    C++

--- OP/lab4/C++/headers/LinearFunction.h
+++ OP/lab4/C++/headers/LinearFunction.h
@@ -7,7 +7,7 @@ class LinearFunction : public TFunction
 public:
     LinearFunction() : TFunction(false) {}
 
-    // Redefine methods since we do not need to change quadratic coefficient in a linear function
+    // Override methods for linear function, since we do not need to manage quadratic coefficient
     void increaseCoefficients(double) override;
     void decreaseCoefficients(double) override;
 };
--- OP/lab4/C++/src/LinearFunction.cpp
+++ OP/lab4/C++/src/LinearFunction.cpp
@@ -1,5 +1,7 @@
 #include "../headers/LinearFunction.h"
 
+// Override methods for linear function, since we do not need to manage quadratic coefficient
+
 void LinearFunction::increaseCoefficients(double scalar)
 {
     m_linear_coeff += scalar;
