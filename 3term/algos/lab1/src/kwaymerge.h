46a0235ba11e697841c5e5a353ddf0aceb3f545b
Dmitri Knysh <dmitryknysh17@gmail.com>
2022-10-10 00:37:50 +0200

    algos.lab1: kwaymerge + raw mmap

--- 3term/algos/lab1/src/kwaymerge.h
+++ 3term/algos/lab1/src/kwaymerge.h
@@ -2,45 +2,50 @@
 #define KWAYMERGESORT_H
 
 #include <iostream>
-#include <fstream>
+#include <iomanip>
+#include <cstdio>
+#include <sys/mman.h> // mmap()
+#include <sys/stat.h> // file size
+#include <unistd.h>   // ftruncate()
 #include <vector>
 #include <string>
 #include <algorithm>
 #include <queue>
-#include <libgen.h> // for basename()
-#include <chrono>   // for time measure
+#include <libgen.h> // basename()
+#include <chrono>   // time measure
 
 using namespace std;
-
 using std::chrono::duration;
 using std::chrono::duration_cast;
 using std::chrono::high_resolution_clock;
 using std::chrono::milliseconds;
 using std::chrono::seconds;
 
+#define fixed_float(x) fixed << setprecision(4) << (x) << defaultfloat
+
 double smart_time_output(
-    const high_resolution_clock::time_point time_start,
-    const high_resolution_clock::time_point time_end);
+    const high_resolution_clock::time_point &time_start,
+    const high_resolution_clock::time_point &time_end);
 string stl_basename(const string &filename);
 
 template <class T>
-class MERGE_RUN_UNIT
+class Merge_Run_Unit
 {
 public:
     T data;
-    istream *stream;
+    FILE *f;
     bool (*compare_function)(const T &x, const T &y);
 
-    MERGE_RUN_UNIT(const T &data,
-                   istream *stream,
+    Merge_Run_Unit(const T &data,
+                   FILE *f,
                    bool (*compare_function)(const T &x, const T &y))
         : data(data),
-          stream(stream),
+          f(f),
           compare_function(compare_function)
     {
     }
 
-    bool operator<(const MERGE_RUN_UNIT &m) const
+    bool operator<(const Merge_Run_Unit &m) const
     {
         // Since priority queue sorts in descending order, we need to negate
         return !(compare_function(data, m.data));
@@ -54,15 +59,15 @@ class KwayMergeSort
 public:
     KwayMergeSort(
         const string &input_file,
-        ostream *out_stream,
+        const string &_output_file,
+        size_t max_buffer_size,
         bool (*compare_function)(const T &x, const T &y) = nullptr,
-        size_t max_buffer_size = 1000000,
         string temp_file = "./temp");
 
     KwayMergeSort(
         const string &input_file,
-        ostream *out_stream,
-        size_t max_buffer_size = 1000000,
+        const string &_output_file,
+        size_t max_buffer_size,
         string temp_file = "./temp");
 
     ~KwayMergeSort(void);
@@ -72,26 +77,26 @@ public:
 private:
     // Constructor originated values
     string _input_file;
-    ostream *_out_stream;
+    string _output_file;
     bool (*_compare_function)(const T &x, const T &y);
     size_t _max_buffer_size;
     string _temp_file;
 
-    bool _temp_file_used;
+    bool _sorted_internally;
     size_t _which_run;
 
     vector<string> _v_temp_file_names; // contains temp file names
-    vector<ifstream *> _v_temp_files;  // contains pointers on opened temp files' streams
+    vector<FILE *> _v_temp_files;      // contains pointers on opened temp files' streams
 
     // Drives the creation of sorted sub-files stored on disk
-    void DivideAndSort();
+    void DistributeAndSort();
 
     // Drives the merging of the sorted temp files.
     // Eventually sorted and merged output is written to `out_stream`
     void Merge();
 
     // Writes sorted chunk of data to a temp file
-    void WriteTempFile(const vector<T> &lines);
+    void WriteTempFile(const uint64_t *buffer_p, const uint64_t bytes);
 
     // Open and close temp files
     void OpenTempFiles();
@@ -101,15 +106,16 @@ private:
 // Constructor
 template <class T>
 KwayMergeSort<T>::KwayMergeSort(const string &input_file,
-                                ostream *out_stream,
-                                bool (*compare_function)(const T &x, const T &y),
+                                const string &output_file,
                                 size_t max_buffer_size,
+                                bool (*compare_function)(const T &x, const T &y),
                                 string temp_file)
     : _input_file(input_file),
-      _out_stream(out_stream),
-      _compare_function(compare_function),
+      _output_file(output_file),
       _max_buffer_size(max_buffer_size),
+      _compare_function(compare_function),
       _temp_file(temp_file),
+      _sorted_internally(false),
       _which_run(0)
 {
 }
@@ -117,14 +123,15 @@ KwayMergeSort<T>::KwayMergeSort(const string &input_file,
 // Constructor without compare function
 template <class T>
 KwayMergeSort<T>::KwayMergeSort(const string &input_file,
-                                ostream *out_stream,
+                                const string &output_file,
                                 size_t max_buffer_size,
                                 string temp_file)
     : _input_file(input_file),
-      _out_stream(out_stream),
+      _output_file(output_file),
       _compare_function(nullptr),
       _max_buffer_size(max_buffer_size),
       _temp_file(temp_file),
+      _sorted_internally(false),
       _which_run(0)
 {
 }
@@ -141,10 +148,10 @@ void KwayMergeSort<T>::Sort()
 {
     double t_das, t_mrg;
     auto t1 = high_resolution_clock::now();
-    DivideAndSort();
+    DistributeAndSort();
     auto t2 = high_resolution_clock::now();
 
-    cout << "DivideAndSort() \t--- ";
+    cout << "DistributeAndSort() \t--- ";
     t_das = smart_time_output(t1, t2);
     cout << "    :- "
          << _which_run
@@ -153,113 +160,104 @@ void KwayMergeSort<T>::Sort()
          << "created.\n\n";
 
     t1 = high_resolution_clock::now();
-    Merge();
+    // Merge();
     t2 = high_resolution_clock::now();
 
     cout << "Merge() \t\t--- ";
     t_mrg = smart_time_output(t1, t2);
     cout << "    :- \n\n";
 
-    cout << "Total time \t\t--- " << t_das + t_mrg << " s" << endl;
+    cout << "Total time \t\t--- " << fixed_float(t_das + t_mrg) << " s" << endl;
 }
 
 template <class T>
-void KwayMergeSort<T>::DivideAndSort()
+void KwayMergeSort<T>::DistributeAndSort()
 {
-    istream *input = new ifstream(_input_file.c_str(), ios::in);
-
-    // Exit if file opened with error (or did not open)
-    if (input->good() == false)
+    FILE *f_in;
+    if ((f_in = fopen(_input_file.c_str(), "rb")) == nullptr)
     {
-        cerr << "Error: Could not open input file (" << _input_file << "). Exiting." << endl;
-        exit(1);
+        cerr << "Error: could not open input file " << _input_file << endl;
+        exit(EXIT_FAILURE);
     }
 
-    vector<T> line_buffer;
-    line_buffer.reserve(100000);
+    fseek(f_in, 0L, SEEK_END);
+    auto f_in_size = ftell(f_in); // input file size
+    fseek(f_in, 0L, SEEK_SET);
 
-    size_t total_bytes = 0; // for tracking the number of consumed bytes
+    uint64_t buffer_size = _max_buffer_size / sizeof(uint64_t);
+    uint64_t *buffer = new (nothrow) uint64_t[buffer_size];
 
-    // Whether or not we used a temp file based on the allocated memory
-    _temp_file_used = false;
-
-    // Read from file line by line until there is no more data
-    T line;
-    while (*input >> line)
+    uint64_t bytes; // number of read bytes
+    while ((bytes = fread(buffer, sizeof(uint64_t), buffer_size, f_in)) > 0)
     {
-        line_buffer.push_back(line);
-        total_bytes += sizeof(line);
+        if (_compare_function != nullptr)
+            sort(buffer, buffer + bytes, *_compare_function);
+        else
+            sort(buffer, buffer + bytes);
 
-        // Sort the buffer and write to a temp file when we have filled up our quota
-        if (total_bytes > _max_buffer_size)
-        {
-            if (_compare_function != nullptr)
-                sort(line_buffer.begin(), line_buffer.end(), *_compare_function);
-            else
-                sort(line_buffer.begin(), line_buffer.end());
+        // If the buffer size is less that input file size (most probably), then distribute between temp files
+        if (_max_buffer_size < f_in_size)
+            WriteTempFile(buffer, bytes);
 
-            // Write the sorted data to a temp file
-            WriteTempFile(line_buffer);
-
-            // and clear the buffer for the next run
-            line_buffer.clear();
-
-            _temp_file_used = true;
-            total_bytes = 0;
-        }
-    }
-
-    // Handle the run (if any) from the last chunk of the input data
-    if (line_buffer.empty() == false)
-    {
-        // Write the last chunk to the temp file
-        // if one had to be used (i.e., we exceeded the memory)
-        if (_temp_file_used == true)
-        {
-            if (_compare_function != nullptr)
-                sort(line_buffer.begin(), line_buffer.end(), *_compare_function);
-            else
-                sort(line_buffer.begin(), line_buffer.end());
-
-            // Write the sorted data to a temp file
-            WriteTempFile(line_buffer);
-            WriteTempFile(line_buffer);
-        }
-
-        // otherwise, the entire file fit in the given memory,
-        // so we can just dump to the output
+        // otherwise the entire file fit in memory, thus sorted in buffer and can be dumped to the output
         else
         {
-            if (_compare_function != nullptr)
-                sort(line_buffer.begin(), line_buffer.end(), *_compare_function);
-            else
-                sort(line_buffer.begin(), line_buffer.end());
+            FILE *f_out;
+            if ((f_out = fopen(_output_file.c_str(), "wb")) == nullptr)
+            {
+                cerr << "Error: could not open output file " << _output_file << endl;
+                fclose(f_in);
+                exit(EXIT_FAILURE);
+            }
 
-            for (size_t i = 0; i < line_buffer.size(); ++i)
-                *_out_stream << line_buffer[i] << endl;
+            fwrite(buffer, sizeof(uint64_t), bytes, f_out);
+            fclose(f_out);
+
+            _sorted_internally = true;
         }
+
+        memset(buffer, 0, bytes);
     }
+
+    fclose(f_in);
+    delete[] buffer;
 }
 
 template <class T>
-void KwayMergeSort<T>::WriteTempFile(const vector<T> &lines)
+void KwayMergeSort<T>::WriteTempFile(const uint64_t *__pbuffer, const uint64_t __bytes)
 {
+    const uint64_t __bytes_size = __bytes * sizeof(uint64_t);
+
     string temp_file_name;
     if (_temp_file.size() == 0)
         temp_file_name = _input_file + "." + to_string(_which_run);
     else
         temp_file_name = _temp_file + "/" + stl_basename(_input_file) + "." + to_string(_which_run);
 
-    ofstream *output = new ofstream(temp_file_name.c_str(), ios::out);
+    FILE *tf_out = fopen(temp_file_name.c_str(), "wb+");
+    int tf_fd = fileno(tf_out);
+    ftruncate(tf_fd, __bytes_size);
 
-    // Write the content of the current buffer to the temp file
-    for (size_t i = 0; i < lines.size(); ++i)
-        *output << lines[i] << endl;
+    uint64_t *tf_mpd = (uint64_t *)mmap(nullptr, __bytes_size, PROT_READ | PROT_WRITE, MAP_SHARED, tf_fd, 0);
 
-    // Update run counter and add the temp file name to the list
+    if (tf_mpd == MAP_FAILED)
+    {
+        fclose(tf_out);
+        cout << "Error mmapping the file\n";
+        exit(EXIT_FAILURE);
+    }
+
+    memcpy(tf_mpd, __pbuffer, __bytes_size);
+
+    if (msync(tf_mpd, __bytes, MS_SYNC) == -1)
+        cerr << "Error: could not sync the file to disk\n";
+
+    // fwrite(__pbuffer, sizeof(uint64_t), __bytes, tf_out);
+
+    munmap(tf_mpd, __bytes);
+    fclose(tf_out);
+
     ++_which_run;
-    output->close();
-    delete output;
     _v_temp_file_names.push_back(temp_file_name);
 }
 
@@ -267,37 +265,44 @@ template <class T>
 void KwayMergeSort<T>::Merge()
 {
     // The merge part can be skipped if previously the entire input file fit
-    // in memory and thus we just dumped to `out_stream` without temp files
-    if (_temp_file_used == false)
+    // in memory and thus was just dumped to `output_file without temp files
+    if (_sorted_internally == true)
         return;
+
+    FILE *f_out;
+    if ((f_out = fopen(_output_file.c_str(), "wb")) == nullptr)
+    {
+        cerr << "Error: could not open output file " << _output_file << endl;
+        exit(EXIT_FAILURE);
+    }
 
     // Open the sorted temp files up for merging
     OpenTempFiles();
 
-    priority_queue<MERGE_RUN_UNIT<T>> out_queue;
+    priority_queue<Merge_Run_Unit<T>> out_queue;
 
-    T line;
+    unsigned char byte;
     for (size_t i = 0; i < _v_temp_files.size(); ++i)
     {
-        *_v_temp_files[i] >> line;
-        out_queue.push(MERGE_RUN_UNIT<T>(line, _v_temp_files[i], _compare_function));
+        fread(&byte, 1, 1, _v_temp_files[i]);
+        out_queue.push(Merge_Run_Unit<T>(static_cast<uint64_t>(byte), _v_temp_files[i], _compare_function));
     }
 
     while (!out_queue.empty())
     {
         // Grab the lowest (first) element
-        MERGE_RUN_UNIT<T> lowest = out_queue.top();
+        Merge_Run_Unit<T> lowest = out_queue.top();
 
-        // output it to the stream (output file)
-        *_out_stream << lowest.data << endl;
+        // output it to the output file
+        fwrite(&lowest.data, 1, 1, f_out);
 
         // and remove from queue
         out_queue.pop();
 
-        // Push the next line from the lowest stream to the queue unless it's EOF
-        *(lowest.stream) >> line;
-        if (*(lowest.stream))
-            out_queue.push(MERGE_RUN_UNIT<T>(line, lowest.stream, _compare_function));
+        // Push the next byte from the lowest stream to the queue unless it's EOF
+        fread(&byte, 1, 1, lowest.f);
+        if (!feof(lowest.f))
+            out_queue.push(Merge_Run_Unit<T>(static_cast<uint64_t>(byte), lowest.f, _compare_function));
     }
 
     // Clean up the temp files
@@ -310,20 +315,20 @@ void KwayMergeSort<T>::OpenTempFiles()
     // Go through each temp file name and open its stream
     for (size_t i = 0; i < _v_temp_file_names.size(); ++i)
     {
-        ifstream *file = new ifstream(_v_temp_file_names[i].c_str(), ios::in);
+        FILE *f;
 
         // Save file stream if opened
-        if (file->good())
-            _v_temp_files.push_back(file);
+        if ((f = fopen(_v_temp_file_names[i].c_str(), "rb")) != nullptr)
+            _v_temp_files.push_back(f);
 
         // Threw an error otherwise
         else
         {
             cerr << "Unable to open temp file ("
                  << _v_temp_file_names[i]
-                 << "). The file might not exist. Exiting."
+                 << "). The file might not exist."
                  << endl;
-            exit(1);
+            exit(EXIT_FAILURE);
         }
     }
 }
@@ -333,10 +338,7 @@ void KwayMergeSort<T>::CloseTempFiles()
 {
     // Close all temp files
     for (size_t i = 0; i < _v_temp_files.size(); ++i)
-    {
-        _v_temp_files[i]->close();
-        delete _v_temp_files[i];
-    }
+        fclose(_v_temp_files[i]);
 
     // and delete them
     for (size_t i = 0; i < _v_temp_file_names.size(); ++i)
@@ -344,25 +346,25 @@ void KwayMergeSort<T>::CloseTempFiles()
 }
 
 double smart_time_output(
-    const high_resolution_clock::time_point time_start,
-    const high_resolution_clock::time_point time_end)
+    const high_resolution_clock::time_point &__tstart,
+    const high_resolution_clock::time_point &__tend)
 {
-    duration<double> seconds = time_end - time_start;
+    duration<double> seconds = __tend - __tstart;
     duration<double, std::milli> ms_double = seconds;
 
     if (ms_double.count() < 1000)
-        cout << ms_double.count() << " ms" << endl;
+        cout << fixed_float(ms_double.count()) << " ms" << endl;
     else
-        cout << seconds.count() << " s" << endl;
+        cout << fixed_float(seconds.count()) << " s" << endl;
 
     return seconds.count();
 }
 
-string stl_basename(const string &path)
+string stl_basename(const string &__path)
 {
     string result;
 
-    char *path_dup = strdup(path.c_str());
+    char *path_dup = strdup(__path.c_str());
     char *basename_part = basename(path_dup);
     result = basename_part;
     free(path_dup);
