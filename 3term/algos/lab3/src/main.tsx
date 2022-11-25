ff7b6ef3c8fa80e15126bb846d2a2d1b7e113313
Dmitri Knysh <dmitryknysh17@gmail.com>
2022-11-25 23:05:09 +0100

    algos.lab3: chore: save table on quit

--- 3term/algos/lab3/src/main.tsx
+++ 3term/algos/lab3/src/main.tsx
@@ -2,4 +2,14 @@ import { render } from "preact";
 import App from "./App";
 import "./index.scss";
 
+import { window, invoke } from "@tauri-apps/api";
+import { TauriEvent } from "@tauri-apps/api/event";
+
+// save last table on app exit
+window.getCurrent().listen(TauriEvent.WINDOW_CLOSE_REQUESTED, () => {
+    invoke("set_working_table", { path: "" }).then(() => {
+        window.getCurrent().close();
+    });
+});
+
 render(<App />, document.getElementById("app") as HTMLElement);
