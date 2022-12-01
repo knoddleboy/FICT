b1551e4454c8677b6b48c1755114624e49fdfcc2
Dmitri Knysh <dmitryknysh17@gmail.com>
2022-12-01 12:57:01 +0100

    algos.lab3: chore: workbench refactor & small fix

--- 3term/algos/lab3/src/components/Workbench/Workbench.tsx
+++ 3term/algos/lab3/src/components/Workbench/Workbench.tsx
@@ -8,6 +8,7 @@ import { MAIN_DATA_DIR } from "../../constants";
 import { AppContext } from "../../App";
 
 import Button from "../Button";
+import TreeView from "../TreeView";
 import { AddIconThin, RemoveIcon, SearchIcon, SettingsIcon } from "../../assets/svg";
 
 import styles from "./Workbench.module.scss";
@@ -22,10 +23,24 @@ type TableData = {
     value: string | null;
 };
 
+type TableDataObject = {
+    data: {
+        key: number;
+        value: string;
+    };
+    left: TableDataObject;
+    right: TableDataObject;
+};
+
 enum TColumn {
     Key = "Key",
     Value = "Value",
     NA = "NA",
+}
+
+enum DataDisplay {
+    Tabular = "Tabular",
+    TreeLike = "TreeLike",
 }
 
 const invokeCtrlA = async (reg: boolean, callback?: () => void) => {
@@ -54,13 +69,11 @@ export const Workbench = () => {
      ********************/
 
     // change table data display look: tabular or treelike
-    const [activeViewButton, setActiveViewButton] = useState(0);
+    const [activeViewButton, setActiveViewButton] = useState<DataDisplay>(DataDisplay.Tabular);
 
     // actual data from a table
     const [data, setData] = useState<TableData[]>([]);
-
-    // parser optimization
-    const prevTable = useRef<FileEntry | undefined>(undefined);
+    const [dataObject, setDataObject] = useState<any>();
 
     const [templateInputRow, setTemplateInputRow] = useState<[TColumn, TableData | null]>([
         TColumn.NA,
@@ -84,6 +97,7 @@ export const Workbench = () => {
      *       REFS       *
      ********************/
 
+    const prevTable = useRef<FileEntry | undefined>(undefined);
     const templateInputRowRef = useRef<HTMLInputElement>(null);
     const searchFieldRef = useRef<HTMLInputElement>(null);
     const deleteButtonRef = useRef<HTMLButtonElement>(null);
@@ -187,23 +201,79 @@ export const Workbench = () => {
         setInvokeDeleteRow(false); // reset delete invoke
     };
 
+    const handleInvokeDeleteRows = () => {
+        //  no need to delete when there are no rows
+        if (!data.length) {
+            return;
+        }
+
+        // on second click: delete selected rows (if any)
+        if (invokeDeleteRow && rowClicked.size) {
+            deleteRows(rowClicked);
+            return;
+        }
+
+        setRegisterDelete(true);
+
+        setInvokeDeleteRow(true);
+
+        // set false to ensure that when deleting rows,
+        // creation does not occur
+        setInvokeAddRow(false);
+    };
+
+    const handleDeleteRows = (idx: TableData) => {
+        if (invokeDeleteRow) {
+            // if `idx` table was already clicked, remove its index ...
+            if (rowClicked.has(idx)) {
+                setRowClicked((prev) => new Set([...prev].filter((i) => i !== idx)));
+                // ... otherwise add its index
+            } else {
+                setRowClicked((prev) => new Set(prev.add(idx)));
+            }
+            return;
+        }
+    };
+
+    const handleGenerateTable = () => {
+        const t = workingTable.value;
+        if (!t) return;
+
+        invoke("generate_table", { path: t.path }).then((res) => {
+            if (res && t.name) {
+                parseTable(t.name);
+            }
+        });
+    };
+
     const handleSearch = (e: KeyboardEvent) => {
         const input = searchFieldRef.current;
         if (!input || e.key !== "Enter") return;
 
         let val = parseInt(input.value);
 
-        if (!val && val !== 0) {
+        if ((!val && val !== 0) || val < 0) {
             alert("Incorrect key.");
             return;
         }
 
         invoke<TableData>("search_item", { key: val }).then((res) => {
             if (res) {
-                alert(`Found item with:\n[key]\n${res.key}\n\n[value]\n${res.value}`);
+                let rv = res.value,
+                    rvSlice = rv,
+                    lim = 200;
+                if (rv && rv.length > lim)
+                    rvSlice = rv.slice(0, lim) + ` <...${rv.length - lim} more...>`;
+                alert(`Found item with:\n[key]\n${res.key}\n\n[value]\n${rvSlice}`);
             } else {
                 alert("Item not found.");
             }
+        });
+    };
+
+    const getAvlObject = () => {
+        invoke<Record<"root", TableDataObject>>("get_avl").then((res) => {
+            setDataObject(res.root);
         });
     };
 
@@ -214,11 +284,18 @@ export const Workbench = () => {
     // update current working table (also used in saving avl tree)
     useEffect(() => {
         invoke("set_working_table", { path: workingTable.value?.path || "" });
+
+        if (activeViewButton === DataDisplay.TreeLike) {
+            getAvlObject();
+        }
     }, [workingTable.value]);
 
     // prevent from parsing the same table as previous
     useEffect(() => {
         if (!workingTable.value) return;
+
+        // no parsing table if is in treelike view
+        if (activeViewButton === DataDisplay.TreeLike) return;
 
         if (prevTable.current === undefined) {
             prevTable.current = {
@@ -235,7 +312,7 @@ export const Workbench = () => {
         }
 
         prevTable.current = workingTable.value;
-    }, [workingTable.value]);
+    }, [workingTable.value, activeViewButton]);
 
     // focus input on a clicked row
     useEffect(() => {
@@ -336,113 +413,101 @@ export const Workbench = () => {
                             <div className={styles.dataValueField}>value</div>
                         </div>
                         <ul className={styles.displayTableRoot} ref={rowsListRef}>
-                            {data.map((d) => {
-                                const idx = d;
-                                return (
-                                    <li
-                                        className={`${styles.dataRow} ${
-                                            rowClicked.has(idx) ? styles.dataRowSelected : ""
-                                        }`}
-                                        key={idx}
-                                        onClick={() => {
-                                            if (invokeDeleteRow) {
-                                                // if `idx` table was already clicked, remove its index ...
-                                                if (rowClicked.has(idx)) {
-                                                    setRowClicked(
-                                                        (prev) =>
-                                                            new Set(
-                                                                [...prev].filter((i) => i !== idx)
-                                                            )
-                                                    );
-                                                    // ... otherwise add its index
-                                                } else {
-                                                    setRowClicked((prev) => new Set(prev.add(idx)));
+                            {activeViewButton === DataDisplay.Tabular ? (
+                                data.map((d) => {
+                                    const idx = d;
+                                    return (
+                                        <li
+                                            className={`${styles.dataRow} ${
+                                                rowClicked.has(idx) ? styles.dataRowSelected : ""
+                                            }`}
+                                            key={idx}
+                                            onClick={() => handleDeleteRows(idx)}
+                                        >
+                                            <div
+                                                className={styles.dataKeyField}
+                                                onDblClick={() =>
+                                                    setTemplateInputRow([TColumn.Key, idx])
                                                 }
-                                                return;
-                                            }
-                                        }}
-                                    >
-                                        <div
-                                            className={styles.dataKeyField}
-                                            onDblClick={() =>
-                                                setTemplateInputRow([TColumn.Key, idx])
-                                            }
-                                        >
-                                            {templateInputRow[0] === TColumn.Key &&
-                                            templateInputRow[1] === idx ? (
-                                                <input
-                                                    type="text"
-                                                    value={d.key ?? ""}
-                                                    onBlur={() =>
-                                                        setTemplateInputRow([TColumn.NA, null])
-                                                    }
-                                                    ref={templateInputRowRef}
-                                                    onKeyDown={(e) => handleInputEnd(e, idx, 0)}
-                                                />
-                                            ) : (
-                                                <span>{d.key}</span>
-                                            )}
-                                        </div>
-                                        <div
-                                            className={styles.dataValueField}
-                                            onDblClick={() =>
-                                                setTemplateInputRow([TColumn.Value, idx])
-                                            }
-                                        >
-                                            {templateInputRow[0] === TColumn.Value &&
-                                            templateInputRow[1] === idx ? (
-                                                <input
-                                                    type="text"
-                                                    value={d.value ?? ""}
-                                                    onBlur={() =>
-                                                        setTemplateInputRow([TColumn.NA, null])
-                                                    }
-                                                    ref={templateInputRowRef}
-                                                    onKeyDown={(e) => handleInputEnd(e, idx, 1)}
-                                                />
-                                            ) : (
-                                                <span>{d.value}</span>
-                                            )}
-                                        </div>
-                                    </li>
-                                );
-                            })}
+                                            >
+                                                {templateInputRow[0] === TColumn.Key &&
+                                                templateInputRow[1] === idx ? (
+                                                    <input
+                                                        type="text"
+                                                        value={d.key ?? ""}
+                                                        onBlur={() =>
+                                                            setTemplateInputRow([TColumn.NA, null])
+                                                        }
+                                                        ref={templateInputRowRef}
+                                                        onKeyDown={(e) => handleInputEnd(e, idx, 0)}
+                                                    />
+                                                ) : (
+                                                    <span>{d.key}</span>
+                                                )}
+                                            </div>
+                                            <div
+                                                className={styles.dataValueField}
+                                                onDblClick={() =>
+                                                    setTemplateInputRow([TColumn.Value, idx])
+                                                }
+                                            >
+                                                {templateInputRow[0] === TColumn.Value &&
+                                                templateInputRow[1] === idx ? (
+                                                    <input
+                                                        type="text"
+                                                        value={d.value ?? ""}
+                                                        onBlur={() =>
+                                                            setTemplateInputRow([TColumn.NA, null])
+                                                        }
+                                                        ref={templateInputRowRef}
+                                                        onKeyDown={(e) => handleInputEnd(e, idx, 1)}
+                                                    />
+                                                ) : (
+                                                    <span>{d.value}</span>
+                                                )}
+                                            </div>
+                                        </li>
+                                    );
+                                })
+                            ) : (
+                                <TreeView tree={dataObject} />
+                            )}
                         </ul>
                         <div className={styles.bottomPanel}>
                             <div className={styles.dataView}>
                                 <Button
                                     background={{
                                         color:
-                                            activeViewButton === 0
+                                            activeViewButton === DataDisplay.Tabular
                                                 ? variables.systemSecondaryDark
                                                 : variables.systemTertiaryDark,
-                                        alpha: activeViewButton === 0 ? 8 : 32,
+                                        alpha: activeViewButton === DataDisplay.Tabular ? 8 : 32,
                                     }}
                                     foregraund={{
-                                        color: activeViewButton === 0 ? "white" : "",
+                                        color:
+                                            activeViewButton === DataDisplay.Tabular ? "white" : "",
                                     }}
-                                    onClick={() => {
-                                        setActiveViewButton(0);
-                                    }}
+                                    onClick={() => setActiveViewButton(DataDisplay.Tabular)}
                                 >
                                     Tabular
                                 </Button>
                                 <Button
                                     background={{
                                         color:
-                                            activeViewButton === 1
+                                            activeViewButton === DataDisplay.TreeLike
                                                 ? variables.systemSecondaryDark
                                                 : variables.systemTertiaryDark,
-                                        alpha: activeViewButton === 1 ? 8 : 32,
+                                        alpha: activeViewButton === DataDisplay.TreeLike ? 8 : 32,
                                     }}
                                     foregraund={{
-                                        color: activeViewButton === 1 ? "white" : "",
+                                        color:
+                                            activeViewButton === DataDisplay.TreeLike
+                                                ? "white"
+                                                : "",
                                     }}
                                     onClick={() => {
-                                        setActiveViewButton(1);
-                                        invoke("get_avl").then((res) => {
-                                            console.log(res);
-                                        });
+                                        setActiveViewButton(DataDisplay.TreeLike);
+                                        getAvlObject();
                                     }}
                                 >
                                     Treelike
@@ -463,26 +528,7 @@ export const Workbench = () => {
                                         color: variables.systemTertiaryDark,
                                         alpha: 32,
                                     }}
-                                    onClick={() => {
-                                        //  no need to delete when there are no rows
-                                        if (!data.length) {
-                                            return;
-                                        }
-
-                                        // on second click: delete selected rows (if any)
-                                        if (invokeDeleteRow && rowClicked.size) {
-                                            deleteRows(rowClicked);
-                                            return;
-                                        }
-
-                                        setRegisterDelete(true);
-
-                                        setInvokeDeleteRow(true);
-
-                                        // set false to ensure that when deleting rows,
-                                        // creation does not occur
-                                        setInvokeAddRow(false);
-                                    }}
+                                    onClick={handleInvokeDeleteRows}
                                     buttonRef={deleteButtonRef}
                                 >
                                     <RemoveIcon size={20} fill={variables.systemSecondaryDark} />
@@ -508,16 +554,7 @@ export const Workbench = () => {
                                         alpha: 32,
                                     }}
                                     className={styles.actionButton}
-                                    onClick={() => {
-                                        const t = workingTable.value;
-                                        if (!t) return;
-
-                                        invoke("generate_table", { path: t.path }).then((res) => {
-                                            if (res && t.name) {
-                                                parseTable(t.name);
-                                            }
-                                        });
-                                    }}
+                                    onClick={handleGenerateTable}
                                 >
                                     <SettingsIcon fill={variables.systemSecondaryDark} />
                                 </Button>
