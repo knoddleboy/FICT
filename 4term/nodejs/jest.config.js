5bda27cbdc0a14a855cf40206c73e44f0f625119
Dmitri Knysh <dmitryknysh17@gmail.com>
2023-03-28 16:03:13 +0200

    feat(nodejs): initial configuration

---
+++ 4term/nodejs/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    projects: [
+        {
+            displayName: "lab2",
+            testMatch: ["<rootDir>/lab2/src/**/*.test.ts"],
+            preset: "ts-jest",
+        },
+    ],
+};
