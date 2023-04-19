1bece981a4b467a137271b3b5ed08bc13e52f371
Dmitri Knysh <dmitryknysh17@gmail.com>
2023-04-20 01:14:39 +0200

    feat(nodejs/lab5): add app

---
+++ 4term/nodejs/lab5/src/app.ts
@@ -0,0 +1,83 @@
+/**
+ * В Node.js з допомогою фреймворка Express напишіть на Typescript веб-сервер, який відповідатиме
+ * стандартам REST і реалізовуватиме базові операції CRUD для сутності user (користувач) із полями
+ * id, username та name:
+ *  - зробіть ендпоінт "створення користувача" з бов'язковим параметром username і необов'язковим
+ *    параметром name;
+ *  - зробіть ендпоінт "отримання даних користувача за його id" (id + username + name);
+ *  - зробіть ендпоінт "список користувачів" (список записів id + username + name);
+ *  - зробіть ендпоінт "оновлення даних користувача за його id";
+ *  - зробіть ендпоінт "видалення користувача за його id";
+ *  - не використовуйте баз даних, зберігайте дані локально в пам'яті процесу або у файловій системі.
+ */
+
+import express, { type Request } from "express";
+import { UserDatabase, type User } from "./userDb";
+
+const userDb = new UserDatabase();
+
+const app = express();
+app.use(express.json());
+
+// Create User endpoint
+app.post("/users", (req: Request<{}, {}, Omit<User, "id">>, res) => {
+    const { username, name } = req.body;
+
+    if (!username) {
+        res.status(400).send("Username is required");
+        return;
+    }
+
+    userDb.addUser(username, name);
+
+    res.status(201).json({ username, name });
+});
+
+// Get User by ID endpoint
+app.get("/users/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const user = userDb.getUserById(id);
+
+    if (!user) {
+        res.status(404).send("User not found");
+        return;
+    }
+
+    res.json(user);
+});
+
+// List Users endpoint
+app.get("/users", (req, res) => {
+    res.json(userDb.getUsers());
+});
+
+// Update User by ID endpoint
+app.put("/users/:id", (req: Request<{ id: string }, {}, Omit<User, "id">>, res) => {
+    const id = parseInt(req.params.id);
+    const { username, name } = req.body;
+
+    const result = userDb.updateUser(id, username, name);
+
+    if (result) {
+        res.status(201).send("User successfully updated");
+    } else {
+        res.status(400).send("User not found");
+    }
+});
+
+// Delete User by ID endpoint
+app.delete("/users/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const result = userDb.deleteUser(id);
+
+    if (result) {
+        res.status(201).send("User successfully deleted");
+    } else {
+        res.status(400).send("User not found");
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
---
+++ 4term/nodejs/lab5/src/userDb.ts
@@ -0,0 +1,44 @@
+export interface User {
+    id: number;
+    username: string;
+    name?: string;
+}
+
+export class UserDatabase {
+    private users: User[] = [];
+
+    addUser(username: string, name?: string) {
+        const id = this.users.length;
+        this.users.push({ id, username, name });
+    }
+
+    getUserById(id: number) {
+        return this.users.find((user) => user.id === id);
+    }
+
+    getUsers() {
+        return this.users;
+    }
+
+    updateUser(id: number, username: string, name?: string) {
+        const userIndex = this.users.findIndex((user) => user.id === id);
+
+        if (userIndex !== -1) {
+            return false;
+        }
+
+        this.users[userIndex] = { id, username, name };
+        return true;
+    }
+
+    deleteUser(id: number) {
+        const userIndex = this.users.findIndex((user) => user.id === id);
+
+        if (userIndex === -1) {
+            return false;
+        }
+
+        this.users.splice(userIndex, 1);
+        return true;
+    }
+}
