2a8da00424aa547837600ffa398488561230289a
Dmitri Knysh <dmitryknysh17@gmail.com>
2023-05-16 14:59:44 +0200

    feat(nodejs/lab6): use `.env` for db

--- 4term/nodejs/lab6/src/lab6.ts
+++ 4term/nodejs/lab6/src/lab6.ts
@@ -1,12 +1,15 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 import { Pool } from "pg";
 import * as query from "./queries";
 
 const pool = new Pool({
-    user: "knysh",
-    host: "localhost",
-    database: "nodejs-lab6",
-    password: "",
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined,
 });
 
 function run(q: string) {
